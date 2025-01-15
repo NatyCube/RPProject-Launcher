fddc1d91d74e15625e9f6531a5610843625f1b48
UTrosh <73399986+UTrosh@users.noreply.github.com>
2025-01-15 20:04:49 +0100

    208

--- src/assets/js/panels/login.js
+++ src/assets/js/panels/login.js
@@ -13,26 +13,22 @@ class Login {
         this.config = config;
         this.db = new database();
 
-        if (typeof this.config.online == 'boolean') {
-            this.config.online ? this.getMicrosoft() : this.getCrack()
-        } else if (typeof this.config.online == 'string') {
-            if (this.config.online.match(/^(http|https):\/\/[^ "]+$/)) {
-                this.getAZauth();
-            }
-        }
-        
         document.querySelector('.cancel-home').addEventListener('click', () => {
             document.querySelector('.cancel-home').style.display = 'none'
             changePanel('settings')
         })
+        document.querySelector('.login-home').style.display = 'block';
+        document.querySelector('.login-offline').style.display = 'block';
+
+        // Initialiser les événements
+        this.initMicrosoftLogin();
+        this.initCrackLogin();
     }
 
-    async getMicrosoft() {
+    initMicrosoftLogin() {
         console.log('Initializing Microsoft login...');
         let popupLogin = new popup();
-        let loginHome = document.querySelector('.login-home');
         let microsoftBtn = document.querySelector('.connect-home');
-        loginHome.style.display = 'block';
 
         microsoftBtn.addEventListener("click", () => {
             popupLogin.openPopup({
@@ -46,7 +42,7 @@ class Login {
                     popupLogin.closePopup();
                     return;
                 } else {
-                    await this.saveData(account_connect)
+                    await this.saveData(account_connect);
                     popupLogin.closePopup();
                 }
 
@@ -57,17 +53,14 @@ class Login {
                     options: true
                 });
             });
-        })
+        });
     }
 
-    async getCrack() {
+    initCrackLogin() {
         console.log('Initializing offline login...');
         let popupLogin = new popup();
-        let loginOffline = document.querySelector('.login-offline');
-
         let emailOffline = document.querySelector('.email-offline');
         let connectOffline = document.querySelector('.connect-offline');
-        loginOffline.style.display = 'block';
 
         connectOffline.addEventListener('click', async () => {
             if (emailOffline.value.length < 3) {
@@ -98,114 +91,27 @@ class Login {
                 });
                 return;
             }
-            await this.saveData(MojangConnect)
+
+            await this.saveData(MojangConnect);
             popupLogin.closePopup();
-        });
-    }
-
-    async getAZauth() {
-        console.log('Initializing AZauth login...');
-        let AZauthClient = new AZauth(this.config.online);
-        let PopupLogin = new popup();
-        let loginAZauth = document.querySelector('.login-AZauth');
-        let loginAZauthA2F = document.querySelector('.login-AZauth-A2F');
-
-        let AZauthEmail = document.querySelector('.email-AZauth');
-        let AZauthPassword = document.querySelector('.password-AZauth');
-        let AZauthA2F = document.querySelector('.A2F-AZauth');
-        let connectAZauthA2F = document.querySelector('.connect-AZauth-A2F');
-        let AZauthConnectBTN = document.querySelector('.connect-AZauth');
-        let AZauthCancelA2F = document.querySelector('.cancel-AZauth-A2F');
-
-        loginAZauth.style.display = 'block';
-
-        AZauthConnectBTN.addEventListener('click', async () => {
-            PopupLogin.openPopup({
-                title: 'Connexion en cours...',
-                content: 'Veuillez patienter...',
-                color: 'var(--color)'
-            });
-
-            if (AZauthEmail.value == '' || AZauthPassword.value == '') {
-                PopupLogin.openPopup({
-                    title: 'Erreur',
-                    content: 'Veuillez remplir tous les champs.',
-                    options: true
-                });
-                return;
-            }
-
-            let AZauthConnect = await AZauthClient.login(AZauthEmail.value, AZauthPassword.value);
-
-            if (AZauthConnect.error) {
-                PopupLogin.openPopup({
-                    title: 'Erreur',
-                    content: AZauthConnect.message,
-                    options: true
-                });
-                return;
-            } else if (AZauthConnect.A2F) {
-                loginAZauthA2F.style.display = 'block';
-                loginAZauth.style.display = 'none';
-                PopupLogin.closePopup();
-
-                AZauthCancelA2F.addEventListener('click', () => {
-                    loginAZauthA2F.style.display = 'none';
-                    loginAZauth.style.display = 'block';
-                });
-
-                connectAZauthA2F.addEventListener('click', async () => {
-                    PopupLogin.openPopup({
-                        title: 'Connexion en cours...',
-                        content: 'Veuillez patienter...',
-                        color: 'var(--color)'
-                    });
-
-                    if (AZauthA2F.value == '') {
-                        PopupLogin.openPopup({
-                            title: 'Erreur',
-                            content: 'Veuillez entrer le code A2F.',
-                            options: true
-                        });
-                        return;
-                    }
-
-                    AZauthConnect = await AZauthClient.login(AZauthEmail.value, AZauthPassword.value, AZauthA2F.value);
-
-                    if (AZauthConnect.error) {
-                        PopupLogin.openPopup({
-                            title: 'Erreur',
-                            content: AZauthConnect.message,
-                            options: true
-                        });
-                        return;
-                    }
-
-                    await this.saveData(AZauthConnect)
-                    PopupLogin.closePopup();
-                });
-            } else if (!AZauthConnect.A2F) {
-                await this.saveData(AZauthConnect)
-                PopupLogin.closePopup();
-            }
         });
     }
 
     async saveData(connectionData) {
         let configClient = await this.db.readData('configClient');
-        let account = await this.db.createData('accounts', connectionData)
-        let instanceSelect = configClient.instance_selct
-        let instancesList = await config.getInstanceList()
+        let account = await this.db.createData('accounts', connectionData);
+        let instanceSelect = configClient.instance_selct;
+        let instancesList = await config.getInstanceList();
         configClient.account_selected = account.ID;
 
         for (let instance of instancesList) {
             if (instance.whitelistActive) {
-                let whitelist = instance.whitelist.find(whitelist => whitelist == account.name)
+                let whitelist = instance.whitelist.find(whitelist => whitelist == account.name);
                 if (whitelist !== account.name) {
                     if (instance.name == instanceSelect) {
-                        let newInstanceSelect = instancesList.find(i => i.whitelistActive == false)
-                        configClient.instance_selct = newInstanceSelect.name
-                        await setStatus(newInstanceSelect.status)
+                        let newInstanceSelect = instancesList.find(i => i.whitelistActive == false);
+                        configClient.instance_selct = newInstanceSelect.name;
+                        await setStatus(newInstanceSelect.status);
                     }
                 }
             }
@@ -217,4 +123,5 @@ class Login {
         changePanel('home');
     }
 }
+
 export default Login;
